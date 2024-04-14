5db2e12fcdb097ecf37a5da757f6be8d401f2acb
DevCrut <68785220+DevCrut@users.noreply.github.com>
2024-04-14 23:09:59 +0000

    Implemented unit tests for all CRUD operations in all services

--- src/test/java/ro/unibuc/hello/service/HelloWorldServiceTest.java
+++ src/test/java/ro/unibuc/hello/service/HelloWorldServiceTest.java
@@ -1,83 +1,123 @@
 package ro.unibuc.hello.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
 import ro.unibuc.hello.data.InformationEntity;
 import ro.unibuc.hello.data.InformationRepository;
+import ro.unibuc.hello.data.ProfessorEntity;
+import ro.unibuc.hello.data.ProfessorRepository;
+import ro.unibuc.hello.data.StudentEntity;
+import ro.unibuc.hello.data.StudentRepository;
 import ro.unibuc.hello.dto.Greeting;
+import ro.unibuc.hello.dto.InformationDTO;
 import ro.unibuc.hello.exception.EntityNotFoundException;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
 class HelloWorldServiceTest {
-
     @Mock
-    InformationRepository mockInformationRepository;
+    private StudentRepository studentRepository;
+    @Mock
+    private ProfessorRepository professorRepository;
 
     @InjectMocks
-    HelloWorldService helloWorldService = new HelloWorldService();
+    private StudentsService studentService;
+    @InjectMocks
+    private ProfessorService professorService;
 
     @Test
-    void test_hello_returnsGreeting(){
-        // Arrange
-        String name = "John";
+    public void testFindStudentByFirstName() {
+        StudentEntity student = new StudentEntity();
+        student.firstName = "Mihai LOL";
+        student.lastName = "LOLOLOLOL";
+        List<StudentEntity> retList = new ArrayList<>();
+        retList.add(student);
+        when(studentRepository.findByFirstName("Mihai LOL")).thenReturn(retList);
 
-        // Act
-        Greeting greeting = helloWorldService.hello(name);
+        // Execute the service call
+        InformationDTO found = studentService.getStudentByName("Mihai LOL");
 
-        // Assert
-        Assertions.assertEquals(1, greeting.getId());
-        Assertions.assertEquals("Hello, John!", greeting.getContent());
+        String retString = student.firstName + " " + student.lastName + " \n\n";
+
+        assertEquals(retString, found.getContent(), "The name of the student should be " + student.firstName + " " + student.lastName + " \n\n");
+    }
+    @Test
+    public void testUpdateOrSaveStudent() {
+        StudentEntity updatedOrSavedStudent = new StudentEntity();
+        updatedOrSavedStudent.id = "SomeSortOfStudentIdIDKWhatItIs";
+        updatedOrSavedStudent.firstName = "Sebi TEST";
+        updatedOrSavedStudent.lastName = "Mustatoiu TEST";
+    
+        when(studentRepository.findById("SomeSortOfStudentIdIDKWhatItIs")).thenReturn(Optional.of(updatedOrSavedStudent));
+        when(studentRepository.save(any(StudentEntity.class))).thenReturn(updatedOrSavedStudent);
+        
+        StudentEntity result = studentService.updateStudent(updatedOrSavedStudent.id, updatedOrSavedStudent);
+        
+        assertEquals("Sebi TEST", result.firstName, "First name should be updated.");
+        assertEquals("Mustatoiu TEST", result.lastName, "Last name should be updated.");
+    }
+    @Test
+    public void testDeleteStudentById() {
+        String studentId = "SomeSortOfStudentIdIDKWhatItIs";
+        doNothing().when(studentRepository).deleteById(studentId);
+        
+        studentService.deleteStudent(studentId);
+        
+        verify(studentRepository).deleteById(studentId);
     }
 
     @Test
-    void test_hello_returnsGreeting_whenNameNull(){
-        // Arrange
+    public void testFindProfessorByFirstName() {
+        ProfessorEntity professor = new ProfessorEntity();
+        professor.firstName = "Andrei TEST";
+        professor.lastName = "Paun TEST";
+        List<ProfessorEntity> retList = new ArrayList<>();
+        retList.add(professor);
+        when(professorRepository.findByFirstName("Andrei TEST")).thenReturn(retList);
 
-        // Act
-        Greeting greeting = helloWorldService.hello(null);
+        // Execute the service call
+        InformationDTO found = professorService.getProfessorByName("Andrei TEST");
 
-        // Assert
-        Assertions.assertEquals(1, greeting.getId());
-        Assertions.assertEquals("Hello, null!", greeting.getContent());
+        String retString = professor.firstName + " " + professor.lastName + " \n\n";
+
+        assertEquals(retString, found.getContent(), "The name of the professor should be " + professor.firstName + " " + professor.lastName + " \n\n");
     }
-
     @Test
-    void test_buildGreetingFromInfo_returnsGreetingWithInformation() {
-        // Arrange
-        String title = "someTitle";
-        InformationEntity informationEntity = new InformationEntity(title, "someDescription");
-
-        when(mockInformationRepository.findByTitle(title)).thenReturn(informationEntity);
-
-        // Act
-        Greeting greeting = helloWorldService.buildGreetingFromInfo(title);
-
-        // Assert
-        Assertions.assertEquals(1, greeting.getId());
-        Assertions.assertEquals("someTitle : someDescription!", greeting.getContent());
+    public void testUpdateOrSaveProfessor() {
+        ProfessorEntity updatedOrSavedProfessor = new ProfessorEntity();
+        updatedOrSavedProfessor.id = "SomeSortOfProfessorIdIDKWhatItIs";
+        updatedOrSavedProfessor.firstName = "Anca TEST";
+        updatedOrSavedProfessor.lastName = "Dobrovat TEST";
+    
+        when(professorRepository.findById("SomeSortOfProfessorIdIDKWhatItIs")).thenReturn(Optional.of(updatedOrSavedProfessor));
+        when(professorRepository.save(any(ProfessorEntity.class))).thenReturn(updatedOrSavedProfessor);
+        
+        ProfessorEntity result = professorService.updateProfessor(updatedOrSavedProfessor.id, updatedOrSavedProfessor);
+        
+        assertEquals("Anca TEST", result.firstName, "First name should be updated.");
+        assertEquals("Dobrovat TEST", result.lastName, "Last name should be updated.");
     }
-
     @Test
-    void test_buildGreetingFromInfo_throwsEntityNotFoundException_whenInformationNull() {
-        // Arrange
-        String title = "someTitle";
-
-        when(mockInformationRepository.findByTitle(title)).thenReturn(null);
-
-        try {
-            // Act
-            Greeting greeting = helloWorldService.buildGreetingFromInfo(title);
-        } catch (Exception ex) {
-            // Assert
-            Assertions.assertEquals(EntityNotFoundException.class, ex.getClass());
-            Assertions.assertEquals("Entity: someTitle was not found", ex.getMessage());
-        }
+    public void testDeleteProfessorById() {
+        String professorId = "SomeSortOfProfessorIdIDKWhatItIs";
+        doNothing().when(professorRepository).deleteById(professorId);
+        
+        professorService.deleteProfessor(professorId);
+        
+        verify(professorRepository).deleteById(professorId);
     }
-
 }
